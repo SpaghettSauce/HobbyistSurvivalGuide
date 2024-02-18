e6e723ccffaa51e2f4ed6ec198b4f2c7a415b7c8
SpaghettiSauce <dontstarve221@gmail.com>
2024-02-19 00:01:15 +0200

    Update BMP_280.ino

--- BMP_280/BMP_280.ino
+++ BMP_280/BMP_280.ino
@@ -1,14 +1,14 @@
 
 #include <Wire.h> // Библиотека для работы с шиной I2C
 #include <SPI.h>  // Библиотека для работы с шиной SPI
-#include <Adafruit_BMP280.h> // Библиотека для работы с датчиком BMP280
+#include <Adafruit_BMP280.h> // Библиотека для работы с датчиком BMP280, скачать:Adafruit.. BMP280_Library,Sensor-master,BusIO
 
 // Для подключения по шине I2C
-Adafruit_BMP280 bmp;
+//Adafruit_BMP280 bmp;
 
 // Для подключения по аппаратному SPI (указываем только номер пина CS)
-#define PIN_CS 10
-Adafruit_BMP280 bmp(PIN_CS);
+//#define PIN_CS 10
+//Adafruit_BMP280 bmp(PIN_CS);
 
 // Для подключения по программному SPI (указываем все пины интерфейса)
 #define PIN_SCK  13
@@ -20,16 +20,16 @@ Adafruit_BMP280 bmp(PIN_CS, PIN_MOSI, PIN_MISO,  PIN_SCK);
 void setup() {
   Serial.begin(9600); // Для вывода отладочной информации в терминал
     if(!bmp.begin()) { // Если датчик BMP280 не найден
-    Serial.println(“BMP280 SENSOR ERROR”); // Выводим сообщение об ошибке
+    Serial.println("BMP280 SENSOR ERROR"); // Выводим сообщение об ошибке
     while(1); // Переходим в бесконечный цикл
   }
 }
 
-bmp.setSampling(Adafruit_BMP280::MODE_NORMAL, // Режим работы
+/*bmp.setSampling(Adafruit_BMP280::MODE_NORMAL, // Режим работы
                 Adafruit_BMP280::SAMPLING_X2, // Точность изм. температуры
                 Adafruit_BMP280::SAMPLING_X16, // Точность изм. давления
                 Adafruit_BMP280::FILTER_X16, // Уровень фильтрации
-                Adafruit_BMP280::STANDBY_MS_500); // Период просыпания, мСек
+                Adafruit_BMP280::STANDBY_MS_500); // Период просыпания, мСек*/
 
 void loop() {
   // Выводим значение температуры
