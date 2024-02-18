ab7048d4bd85ab617406b20ea6aba04f12b4ff3a
SpaghettiSauce <dontstarve221@gmail.com>
2024-02-18 23:14:32 +0200

    sensors

---
+++ BMP_280/BMP_280.ino
@@ -0,0 +1,49 @@
+
+#include <Wire.h> // Библиотека для работы с шиной I2C
+#include <SPI.h>  // Библиотека для работы с шиной SPI
+#include <Adafruit_BMP280.h> // Библиотека для работы с датчиком BMP280
+
+// Для подключения по шине I2C
+Adafruit_BMP280 bmp;
+
+// Для подключения по аппаратному SPI (указываем только номер пина CS)
+#define PIN_CS 10
+Adafruit_BMP280 bmp(PIN_CS);
+
+// Для подключения по программному SPI (указываем все пины интерфейса)
+#define PIN_SCK  13
+#define PIN_MISO 12
+#define PIN_MOSI 11
+#define PIN_CS   10
+Adafruit_BMP280 bmp(PIN_CS, PIN_MOSI, PIN_MISO,  PIN_SCK);
+
+void setup() {
+  Serial.begin(9600); // Для вывода отладочной информации в терминал
+    if(!bmp.begin()) { // Если датчик BMP280 не найден
+    Serial.println(“BMP280 SENSOR ERROR”); // Выводим сообщение об ошибке
+    while(1); // Переходим в бесконечный цикл
+  }
+}
+
+bmp.setSampling(Adafruit_BMP280::MODE_NORMAL, // Режим работы
+                Adafruit_BMP280::SAMPLING_X2, // Точность изм. температуры
+                Adafruit_BMP280::SAMPLING_X16, // Точность изм. давления
+                Adafruit_BMP280::FILTER_X16, // Уровень фильтрации
+                Adafruit_BMP280::STANDBY_MS_500); // Период просыпания, мСек
+
+void loop() {
+  // Выводим значение температуры
+  Serial.print(F("Temperature = "));
+  Serial.print(bmp.readTemperature()); // Функция измерения температуры
+  Serial.println(" *C");
+
+  // Выводим значение атмосферного давления
+  Serial.print(F("Pressure = "));
+  Serial.print(bmp.readPressure());  // Функция измерения атм. давления
+  Serial.println(" Pa");
+
+  // Выводим значение высоты
+  Serial.print(F("Approx altitude = "));
+  Serial.print(bmp.readAltitude(1013.25)); // Функция измерения высоты
+  Serial.println(" m");
+}
