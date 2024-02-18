39e2ce0dd12c4c510a1f9dbe45c624c4bff18a31
SpaghettiSauce <dontstarve221@gmail.com>
2024-02-19 00:44:14 +0200

    Update MPU_9250.ino

--- MPU_9250/MPU_9250.ino
+++ MPU_9250/MPU_9250.ino
@@ -1,174 +1,120 @@
-#include <Wire.h> //Уже встроенная библиотека
-#include <TimerOne.h> //библиотека для работы с таймерами, нужно скачивать(см libraries manager)
-
-#define MPU9250_ADDRESS 0x68
-#define MAG_ADDRESS 0x0C
-
-#define GYRO_FULL_SCALE_250_DPS 0x00 
-#define GYRO_FULL_SCALE_500_DPS 0x08
-#define GYRO_FULL_SCALE_1000_DPS 0x10
-#define GYRO_FULL_SCALE_2000_DPS 0x18
-
-#define ACC_FULL_SCALE_2_G 0x00 
-#define ACC_FULL_SCALE_4_G 0x08
-#define ACC_FULL_SCALE_8_G 0x10
-#define ACC_FULL_SCALE_16_G 0x18
-
-// This function read Nbytes bytes from I2C device at address Address. 
-// Put read bytes starting at register Register in the Data array. 
-void I2Cread(uint8_t Address, uint8_t Register, uint8_t Nbytes, uint8_t* Data)
-    {
-      // Set register address
-      Wire.beginTransmission(Address);
-      Wire.write(Register);
-      Wire.endTransmission();
-
-      // Read Nbytes
-      Wire.requestFrom(Address, Nbytes); 
-      uint8_t index=0;
-      while (Wire.available())
-      Data[index++]=Wire.read();
-    }
-
-// Write a byte (Data) in device (Address) at register (Register)
-void I2CwriteByte(uint8_t Address, uint8_t Register, uint8_t Data)
-    {
-      // Set register address
-      Wire.beginTransmission(Address);
-      Wire.write(Register);
-      Wire.write(Data);
-      Wire.endTransmission();
-    }
-
-// Initial time
-long int ti;
-volatile bool intFlag=false;
-
-// Initializations
+#include <LiquidCrystal_I2C.h>    //скачать, нужно для экранчика с шиной i2c
+#include <MPU9250.h>              //надо скачивать, основная библиотека для работы с сенсором
+#include <Wire.h>                 //уже есть, библиотека для работы с шинаи I2C
+ 
+LiquidCrystal_I2C lcd(0x27, 16, 2); // задание параметров дисплея, 
+                                    //адрес I2C-конвертера 0х27 или 0х3f
+ 
+MPU9250 mpu;                      // Объявляем переменную для датчика
+ 
 void setup()
-    {
-      // Arduino initializations
-      Wire.begin();
-      Serial.begin(115200);
-
-      // Set accelerometers low pass filter at 5Hz
-      I2CwriteByte(MPU9250_ADDRESS,29,0x06);
-      // Set gyroscope low pass filter at 5Hz
-      I2CwriteByte(MPU9250_ADDRESS,26,0x06);
-
-      // Configure gyroscope range
-      I2CwriteByte(MPU9250_ADDRESS,27,GYRO_FULL_SCALE_1000_DPS);
-      // Configure accelerometers range
-      I2CwriteByte(MPU9250_ADDRESS,28,ACC_FULL_SCALE_4_G);
-      // Set by pass mode for the magnetometers
-      I2CwriteByte(MPU9250_ADDRESS,0x37,0x02);
-
-      // Request continuous magnetometer measurements in 16 bits
-      I2CwriteByte(MAG_ADDRESS,0x0A,0x16);
-
-      pinMode(13, OUTPUT);
-      Timer1.initialize(10000); // initialize timer1, and set a 1/2 second period
-      Timer1.attachInterrupt(callback); // attaches callback() as a timer overflow interrupt
-
-      // Store initial time
-      ti=millis();
-    } 
-
-// Counter
-long int cpt=0;
-
-void callback()
-    { 
-      intFlag=true;
-      digitalWrite(13, digitalRead(13) ^ 1);
-    }
-
-// Main loop, read and display data
+{
+  Serial.begin(115200);           //Запускаем монитор порта
+  Wire.begin();                   //Запускаем работу с шиной I2C
+  delay(2000);
+  mpu.setup(0x68);                   //Адрес датчика на шине I2C
+  mpu.setMagneticDeclination(7.4);  //в скобках необходимо указать магнинтное отклонение
+  delay(5000);
+ 
+//Калибровка датчика. Запускаем монитор по последовательному соединению 
+//и следуем инструкции
+  Serial.println("Accel Gyro calibration will start in 5sec.");               
+  Serial.println("Please leave the device still on the flat plane.");         
+  mpu.verbose(true);
+  delay(5000);
+  mpu.calibrateAccelGyro();
+ 
+  Serial.println("Mag calibration will start in 5sec.");
+  Serial.println("Please Wave device in a figure eight until done.");
+  delay(5000);
+  mpu.calibrateMag();
+ 
+  print_calibration();
+  mpu.verbose(false);
+ 
+  //экран
+  lcd.init();                        // запуск дисплея
+  lcd.backlight();                   // включение подсветки дисплея
+  lcd.setCursor(0, 0);               // установка курсора в ячейку на дисплее
+  lcd.print("x");                    // вывод на экран
+  lcd.setCursor(6, 0);
+  lcd.print("y");
+  lcd.setCursor(11, 0);
+  lcd.print("z");
+  lcd.setCursor(0, 1);
+  lcd.print("ax:");
+  lcd.setCursor(8, 1);
+  lcd.print("ay:");
+}
+ 
 void loop()
-    {
-      while (!intFlag);
-      intFlag=false;
-
-      // Display time
-      Serial.print (millis()-ti,DEC);
-      Serial.print ("t");
-
-      // _______________
-      // ::: Counter :::
-
-      // Display data counter
-      // Serial.print (cpt++,DEC);
-      // Serial.print ("t");
-
-      // ____________________________________
-      // ::: accelerometer and gyroscope :::
-
-      // Read accelerometer and gyroscope
-      uint8_t Buf[14];
-      I2Cread(MPU9250_ADDRESS,0x3B,14,Buf);
-
-      // Create 16 bits values from 8 bits data
-
-      // Accelerometer
-      int16_t ax=-(Buf[0]<<8 | Buf[1]);
-      int16_t ay=-(Buf[2]<<8 | Buf[3]);
-      int16_t az=Buf[4]<<8 | Buf[5];
-
-      // Gyroscope
-      int16_t gx=-(Buf[8]<<8 | Buf[9]);
-      int16_t gy=-(Buf[10]<<8 | Buf[11]);
-      int16_t gz=Buf[12]<<8 | Buf[13];
-
-      // Display values
-
-     // Accelerometer
-     Serial.print (ax,DEC); 
-     Serial.print ("t");
-     Serial.print (ay,DEC);
-     Serial.print ("t");
-     Serial.print (az,DEC); 
-     Serial.print ("t");
-
-     // Gyroscope
-     Serial.print (gx,DEC); 
-     Serial.print ("t");
-     Serial.print (gy,DEC);
-     Serial.print ("t");
-     Serial.print (gz,DEC); 
-     Serial.print ("t");
-
-     // _____________________
-     // ::: Magnetometer :::
-
-     // Read register Status 1 and wait for the DRDY: Data Ready
-
-     uint8_t ST1;
-     do
-         {
-           I2Cread(MAG_ADDRESS,0x02,1,&ST1);
-         }
-     while (!(ST1&0x01));
-
-     // Read magnetometer data 
-     uint8_t Mag[7]; 
-     I2Cread(MAG_ADDRESS,0x03,7,Mag);
-
-     // Create 16 bits values from 8 bits data
-
-     // Magnetometer
-     int16_t mx=-(Mag[3]<<8 | Mag[2]);
-     int16_t my=-(Mag[1]<<8 | Mag[0]);
-     int16_t mz=-(Mag[5]<<8 | Mag[4]);
-
-     // Magnetometer
-     Serial.print (mx+200,DEC); 
-     Serial.print ("t");
-     Serial.print (my-70,DEC);
-     Serial.print ("t");
-     Serial.print (mz-700,DEC); 
-     Serial.print ("t");
-
-     // End of line
-     Serial.println("");
-     // delay(100); 
-   }
+{
+  static uint32_t prev_ms = millis();
+  if ((millis() - prev_ms) > 100)
+  {
+    mpu.update();
+ 
+    prev_ms = millis();
+ 
+    lcd.setCursor(1, 0);
+// выводим на экран округленное значение по оси X
+    lcd.print(int(mpu.getRoll()));       
+    lcd.print(" ");
+    lcd.setCursor(7, 0);
+// выводим на экран округленное значение по оси Y
+    lcd.print(int(mpu.getPitch()));      
+    lcd.print(" ");
+    lcd.setCursor(12, 0);
+// выводим на экран округленное значение по оси Z
+    lcd.print(int(mpu.getYaw()));        
+    lcd.print(" ");
+    lcd.setCursor(3, 1);
+ 
+// задаем переменную для ускорения по оси X
+// выводим на экран округленное значение ускорения в размерности g*1000
+    float ux = mpu.getAcc(0) * 1000;     
+    lcd.print(int(ux));                  
+    lcd.print("  ");
+ 
+// задаем переменную для ускорения по оси Y
+// выводим на экран округленное значение ускорение в размерности g*1000
+    lcd.setCursor(11, 1);
+    float uy = mpu.getAcc(1) * 1000;     
+    lcd.print(int(uy));                  
+    lcd.print("  ");
+    delay(50);
+ 
+  }
+}
+ 
+void print_calibration() { //функция выводящая откалибрированнае данные на экран
+  Serial.println("< calibration parameters >");
+  Serial.println("accel bias [g]: ");
+  Serial.print(mpu.getAccBiasX() * 1000.f / (float)MPU9250::CALIB_ACCEL_SENSITIVITY);
+  Serial.print(", ");
+  Serial.print(mpu.getAccBiasY() * 1000.f / (float)MPU9250::CALIB_ACCEL_SENSITIVITY);
+  Serial.print(", ");
+  Serial.print(mpu.getAccBiasZ() * 1000.f / (float)MPU9250::CALIB_ACCEL_SENSITIVITY);
+  Serial.println();
+  Serial.println("gyro bias [deg/s]: ");
+  Serial.print(mpu.getGyroBiasX() / (float)MPU9250::CALIB_GYRO_SENSITIVITY);
+  Serial.print(", ");
+  Serial.print(mpu.getGyroBiasY() / (float)MPU9250::CALIB_GYRO_SENSITIVITY);
+  Serial.print(", ");
+  Serial.print(mpu.getGyroBiasZ() / (float)MPU9250::CALIB_GYRO_SENSITIVITY);
+  Serial.println();
+  Serial.println("mag bias [mG]: ");
+  Serial.print(mpu.getMagBiasX());
+  Serial.print(", ");
+  Serial.print(mpu.getMagBiasY());
+  Serial.print(", ");
+  Serial.print(mpu.getMagBiasZ());
+  Serial.println();
+  Serial.println("mag scale []: ");
+  Serial.print(mpu.getMagScaleX());
+  Serial.print(", ");
+  Serial.print(mpu.getMagScaleY());
+  Serial.print(", ");
+  Serial.print(mpu.getMagScaleZ());
+  Serial.println();
+}
