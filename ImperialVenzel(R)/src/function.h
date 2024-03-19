406c1b92ecc2f94fc3e83e69bad118695b6ed958
SpaghettiSauce <dontstarve221@gmail.com>
2024-03-19 22:09:29 +0200

    bye

--- ImperialVenzel(R)/src/function.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include <Arduino.h>
-#include <Servo.h>
-int angleMove(int potentPIN);
-void damageControl(int &lives, int PHS, int currentTime, int startingTime, int BUZZER);
-void lifeSystem(int N, int pins[], int &lives/* int &lives, int lifePIN1, int lifePIN2, int lifePIN3*/);
