22cf747940780ed80716b8ca97d6b7a53fcb38b6
Даня Ходосевич <dontstarve221@gmail.com>
2024-09-22 13:09:20 +0200

    funny car end

--- Projects/FynniCar/src/main.cpp
+++ Projects/FynniCar/src/main.cpp
@@ -1,28 +1,56 @@
 #include <Arduino.h>
+
+#define motorR 9
+#define motorL 10
 int val;
-int LED = 10; 
 
-
-
-void setup() {
-  Serial.begin(9600);
-  pinMode(LED,OUTPUT);
-  digitalWrite(LED,HIGH);
+void setup() 
+{
+  Serial.begin(115200);
+  pinMode(motorR, OUTPUT);
+  pinMode(motorL, OUTPUT);
   pinMode(0, INPUT_PULLUP);
 }
 
-void loop() {
-  if (Serial.available())
-  {
-    val = Serial.read();
-    if (val == '1')
-    {
-      digitalWrite(LED,HIGH);
-    }
-    if (val == '0')
-    {
-      digitalWrite(LED,LOW);
-    }
+void loop() 
+{
+  int speed = 255; 
+  
+  if (Serial.available() > 0) 
+  {  
+    val = Serial.read();          
+    Serial.println(val);           
   }
 
+    if (val == 'd') 
+    {  // Move to the right
+      analogWrite(motorR, speed - 180);
+      analogWrite(motorL, speed);
+      Serial.println("Moving forward");
+    }
+
+    else if (val == 'w') 
+    {  // Move forward
+      analogWrite(motorR, speed);
+      analogWrite(motorL, speed);
+      Serial.println("Moving forward");}
+
+    else if (val == 'a') 
+    {  // Move to the left
+      analogWrite(motorR, speed);
+      analogWrite(motorL, speed - 180);
+      Serial.println("Moving forward");}
+
+    else if (val == 's') 
+    {  // stop
+      analogWrite(motorR, -speed);
+      analogWrite(motorL, -speed);
+      Serial.println("Moving backward");
+    } 
+    else 
+    {
+      Serial.println("What does that supposed to mean?");
+    }
+    
+  delay(100);  
 }
