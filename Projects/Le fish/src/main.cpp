e5da50f6a7f8093e156d19873bda5fa220c17b37
Даня Ходосевич <114701605+SpaghettSauce@users.noreply.github.com>
2024-11-14 01:13:13 +0200

    final(?)fish commit

--- Projects/Le fish/src/main.cpp
+++ Projects/Le fish/src/main.cpp
@@ -3,35 +3,43 @@
 #include "RedMP3.h"
 #include <Arduino.h>
 
+#define fish 6
+#define french 12
+#define cover 13
+
 #define MP3_RX 4//RX of Serial MP3 module connect to D4 of Arduino
 #define MP3_TX 2//TX to D2, note that D2 can not be used as RX on Mega2560, you should modify this if you donot use Arduino UNO
 MP3 mp3(MP3_RX, MP3_TX);
 
 int8_t index  = 0x01;//the first song in the TF card
-int8_t volume = 0x1d;//0~0x1e (30 adjustable level)
+int8_t volume = 0x0D;//0~0x1e (30 adjustable level)
 
-const int buttonPin1 = 12;
-const int buttonPin2 = 13;
 
 void setup()
 {
-  pinMode(buttonPin1, INPUT_PULLUP);
-  pinMode(buttonPin2, INPUT_PULLUP);
+
+  pinMode(cover, INPUT_PULLUP);
+  pinMode(french, INPUT_PULLUP);
   delay(500);
 }
 
 void loop()
 {
-  int buttonState1 = digitalRead(buttonPin1);
-  int buttonState2 = digitalRead(buttonPin2);
+  int speed = 150;
+  int buttonState1 = digitalRead(cover);
+  int buttonState2 = digitalRead(french);
 
-   if (buttonState1 == LOW) { 
+   if (french == LOW) { 
+    analogWrite(fish,speed);
+
     int8_t index  = 0x01;
     mp3.playWithVolume(index,volume);
     delay(50);
   }
 
-  if (buttonState2 == LOW) { 
+  if (cover == LOW) { 
+    speed -= 100;
+    analogWrite(fish,speed);
     int8_t index  = 0x02;
     mp3.playWithVolume(index,volume);
     delay(50);
