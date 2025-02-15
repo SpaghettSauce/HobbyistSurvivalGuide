7f699b49e10706521d8b4d7cdff6d70489f0b552
Даня Ходосевич <dontstarve221@gmail.com>
2025-02-15 13:48:36 +0200

    Update FunnyShipINO.ino

--- Projects/FynniShip/FunnyShipINO/FunnyShipINO.ino
+++ Projects/FynniShip/FunnyShipINO/FunnyShipINO.ino
@@ -16,7 +16,15 @@
 #define pump4_1 7
 #define pump4_2 6
 
-#define valve 7
+#define valve0 0
+#define valve1 1
+#define valve2 2
+#define valve3 3
+#define valve4 4
+#define valve5 5
+#define valve6 6
+#define valve7 7
+
 constexpr auto pinCS = 6;
 L3G gyro;
 Octofet octofet(pinCS);
@@ -38,53 +46,81 @@ void setup() {
   pinMode(pump4_1, OUTPUT);
   pinMode(pump4_2,OUTPUT);
   Serial.begin(9600);
- // Wire.begin();
+  Wire.begin();
 
-  /*if (!gyro.init())
+  if (!gyro.init())//Проверка подключения гироскопа к ардуино 
   {
     Serial.println("Failed to autodetect gyro type!");
     while (1);
   }
 
-  gyro.enableDefault();*/
+  gyro.enableDefault();
 }
 
 void loop() {
- // gyro.read();
-  octofet.digitalWrite(valve,HIGH);
-  analogWrite(pump1_1, 255);
-  analogWrite(pump1_2,0);
-  delay(500);
-  analogWrite(pump1_1, 0);
-  analogWrite(pump1_2,0);
-  octofet.digitalWrite(valve, LOW);
-  delay(500);
-  analogWrite(pump1_1, 255);
-  analogWrite(pump1_2,0);
-  delay(500);
+  gyro.read();
 
-  /*
-  analogWrite(pump2_1, 255);
-  analogWrite(pump2_2,0);
+  if (map((int)gyro.g.x,-80 , 40 , 0 , 10 ) >=  100)
+  {
+    octofet.digitalWrite(valve0,HIGH);
+    analogWrite(pump1_1, 255);
+    analogWrite(pump1_2,0);
+    delay (500);
 
-  analogWrite(pump3_1, 255);
-  analogWrite(pump3_2,0);
+    analogWrite(pump1_1, 0);
+    analogWrite(pump1_2,255);
+    
+    octofet.digitalWrite(valve0,LOW);
+    octofet.digitalWrite(valve1,HIGH);
+  }
 
-  analogWrite(pump4_1, 0);
-  analogWrite(pump4_2,255);
+ if (map((int)gyro.g.x,-80 , 40 , 0 , 10 ) <=  100)
+  {
+    octofet.digitalWrite(valve2,HIGH);
+    analogWrite(pump2_1, 255);
+    analogWrite(pump2_2,0);
+    delay (500);
 
-  /*Serial.print("G ");
+    analogWrite(pump2_1, 0);
+    analogWrite(pump2_2,255);
+    
+    octofet.digitalWrite(valve2,LOW);
+    octofet.digitalWrite(valve3,HIGH);
+  }
+   if (map((int)gyro.g.x,-80 , 40 , 0 , 10 ) >=  50)
+  {
+    octofet.digitalWrite(valve4,HIGH);
+    analogWrite(pump3_1, 255);
+    analogWrite(pump3_2,0);
+    delay (500);
+
+    analogWrite(pump3_1, 0);
+    analogWrite(pump3_2,255);
+    
+    octofet.digitalWrite(valve4,LOW);
+    octofet.digitalWrite(valve5,HIGH);
+  }
+   if (map((int)gyro.g.x,-80 , 40 , 0 , 10 ) <=  50)
+  {
+    octofet.digitalWrite(valve6,HIGH);
+    analogWrite(pump4_1, 255);
+    analogWrite(pump4_2,0);
+    delay (500);
+
+    analogWrite(pump4_1, 0);
+    analogWrite(pump4_2,255);
+    
+    octofet.digitalWrite(valve6,LOW);
+    octofet.digitalWrite(valve7,HIGH);
+  }
+
+  Serial.print("G ");
   Serial.print("X: ");
   Serial.print(map((int)gyro.g.x,-500 , 500 , 0 , 100 )); 
   Serial.print(" Y: ");
   Serial.print(map((int)gyro.g.y,-500 , 500 , 0 , 100 ));      
   Serial.print(" Z: ");
   Serial.println(map((int)gyro.g.z,-500 , 500 , 0 , 100 )); 
-  if (map((int)gyro.g.x,-80 , 40 , 0 , 10 ) >=  100)
-  {
-    analogWrite(pump1, 255);
-    analogWrite(pump2,0);
-    delay (500);
-  }
-  delay(100);*/ 
+
+  delay(100);
 }
