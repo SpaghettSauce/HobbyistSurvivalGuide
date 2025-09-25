46cdba2e954cf8f2bfba84c3228a74baade4edd7
Даня Ходосевич <114701605+SpaghettSauce@users.noreply.github.com>
2025-09-25 23:46:26 +0200

    wafel easy

--- Projects/WIFel/WIFel INO/sketch_sep25a/sketch_sep25a.ino
+++ Projects/WIFel/WIFel INO/sketch_sep25a/sketch_sep25a.ino
@@ -1,18 +1,11 @@
 #include <Servo.h>
-
-#define PERIOD 500
-#define MAX 180.0
 #define BUT1 7
+Servo Servik;
 
-uint32_t time;
-
-Servo ServikR;
-Servo ServikL;
 
 void setup() {
 
-  ServikR.attach(8);
-  ServikL.attach(9);
+  Servik.attach(8);
   pinMode(BUT,INPUT_PULLUP);
 
 }
@@ -21,13 +14,9 @@ void loop() {
   boolean butisup = digitalRead(BUT);
 
   if(butisup) {
-
-    if (millis() - time >= PERIOD) {  /
-    time = millis();          
-    ServikR.write(180);
-    ServikL.write(0);
-    }
-    butisup = !butisup;
+    Servik.write(180);
+    delay(200);
+    Servik.write(0);
   }
         
 
